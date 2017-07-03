5bd742d9bb09e435876292f9aea30f279f33bf89
mars <578595193@qq.com>
2017-07-03 10:14:44 +0800

    兼容浏览器

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function sortAlphabetWithKey(key) {
 	var tmp_alphabet = alphabet.split('').sort(function () {
 		index++
 		if (index % 2 === 0) {
-			return key[index % key.length] >= alphabet[index]
+			return key[index % key.length] >= alphabet[index] ? 1 : -1
 		}
 		return key[index % key.length] < alphabet[index]
 	})
